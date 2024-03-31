0190ed5ca7f78327216230476c4fb9cf78531f37
Benjamin <by82@cornell.edu>
2024-03-31 18:16:14 -0400

    added NN

---
+++ NN/nnet1.py
@@ -0,0 +1,129 @@
+from __future__ import absolute_import, division, print_function
+
+# tf and keras
+import tensorflow as tf
+#from tensorflow import keras
+import keras
+#from keras.models import model_from_json
+
+# helper libs
+import numpy as np
+import matplotlib.pyplot as plt
+import ast
+import Blackjack as bj
+
+# introduction
+print( "TensorFlow Version: " + tf.__version__ )
+
+
+
+##############
+#preprocessing the level 1 data set
+################
+# get the data set
+# data = open( "data_sets/blackjack2-2-out.data").readlines()
+# tags = open( "data_sets/blackjack2-2-out.tags").readlines()
+data = open( "data_sets/blackjack1-2-out.data").readlines()
+tags = open( "data_sets/blackjack1-2-out.tags").readlines()
+data_clean = []
+tags_clean = []
+
+#strip whitespaces and such
+first = True
+i = 0
+for datum in data:
+
+	# skip empty line first
+	if first:
+		first = False
+		continue
+	clean_datum = datum[:datum.index('\n')].strip()
+	clean_datum = ast.literal_eval(clean_datum)
+	if type(clean_datum) is int:
+		data_clean = data_clean + [ clean_datum ]
+	else:
+	  data_clean = data_clean + clean_datum 
+		
+
+
+first = True
+for tag in tags:
+	if first:
+		first = False
+		continue
+	tag = tag[:tag.index('\n')]
+	if tag == "h":
+		tags_clean = tags_clean + [ 1.0 ]
+	else:
+		tags_clean = tags_clean + [ 0.0 ]
+
+size = int( len(data)*(0.75) )
+
+num_features = 2  
+
+train_data = np.array( data_clean[1:size] )
+print(train_data)
+train_tags = np.array( tags_clean[1:size] )
+print(train_tags)
+test_data = np.array( data_clean[size:] )
+print(test_data)
+test_tags = np.array( tags_clean[size:] )
+
+################
+#train
+##############
+
+#The model in this example a dense 2-layer neurel network. 
+#The first layer contained 4096 neurons, while the second only had two, for 'hit' or 'stay.' 
+#The 'adam' optimizer was used, with a loss of 'sparse_categorical_crossentropy.' 
+#Training and testing data was split 50/50 randomly. There are 10 epochs.
+model = keras.Sequential()
+model.add( keras.layers.Dense(4096, input_dim=2) )
+model.add( keras.layers.Dense(2, activation=tf.nn.softmax) )
+model.compile(optimizer='adam',
+	loss='sparse_categorical_crossentropy',
+	metrics=['accuracy'])
+model.fit(train_data, train_tags, epochs=10)
+test_loss, test_acc = model.evaluate(test_data, test_tags)
+print('Test accuracy:', test_acc)
+
+
+#The model will output the accuracy of the model, using a random 25% of input as test cases. The model can then be saved via
+# save model
+# taken from https://machinelearningmastery.com/save-load-keras-deep-learning-models/
+model_json = model.to_json()
+with open( "models/blackjackmodel.1.json", "w") as json_file:
+	json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("models/blackjackmodel.1.h5")
+print( "Model saved" )
+
+
+#To find a heuristic, hand values from 2-21 were tested on the classifier. To do this we need to first deserialize the model from its file
+# open serialized model
+# taken from https://machinelearningmastery.com/save-load-keras-deep-learning-models/
+json_file = open('models/blackjackmodel.1.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = keras.models.model_from_json( loaded_model_json, custom_objects={"GlorotUniform": tf.keras.initializers.glorot_uniform} )
+model.load_weights( "models/blackjackmodel.1.h5" )
+print( "Model loaded from disk" )
+
+
+
+print( "testing model" )
+
+for i in range(21):
+	prediction = model.predict( np.array([ [i,10] ]) )
+	if prediction[0][0] > prediction[0][1]:
+		print( str(i) + " stay" )
+	else:
+		print( str(i) + " hit" )
+		
+
+wins, losses, ties = bj.test_model( "blackjackmodel.1", 10000, True, 1, False )
+total = wins + losses + ties
+win_percentage = (wins/total)*100.0
+loss_percentage = (losses/total)*100.0
+tie_percentage = (ties/total)*100.0
+print( "Percentage won:  " + str( win_percentage ) )
--- train.py
+++ train.py
@@ -1,2 +1,181 @@
 import numpy as np
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class BlackjackNNWithCount(nn.Module):
+    def init(self):
+        super(BlackjackNNWithCount, self).init()
+        self.fc1 = nn.Linear(4, 128)  # 4 inputs: player hand, dealer's card, usable ace, card count
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 1)   # 1 output: probability of hitting
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.sigmoid(self.fc3(x))
+        return x
+    
+model = BlackjackNN()
+criterion = nn.BCELoss()  # Binary Cross-Entropy Loss for binary classification
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(num_epochs):
+    for game_states, decisions in data_loader:
+        optimizer.zero_grad()
+        outputs = model(game_states)
+        loss = criterion(outputs, decisions)
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(dataloader)}")
+
+
+    print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+
+
+
+
+
+
+import gym
+import torch.nn as nn
+import torch
+import numpy as np
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from torch.optim import optimizer
+
+def train(learner, observations, actions, num_epochs=100):
+    """Train function for learning a new policy using BC.
+    
+    Parameters:
+        learner (Learner)
+            A Learner object (policy)
+        observations (list of numpy.ndarray)
+            A list of numpy arrays of shape (7166, 11, ) 
+        actions (list of numpy.ndarray)
+            A list of numpy arrays of shape (7166, 3, )
+        num_epochs (int)
+            Number of epochs to run the train function for
+    
+    Returns:
+        learner (Learner)
+            A Learner object (policy)
+    """
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(learner.parameters(), lr=3e-4)
+    dataset = TensorDataset(torch.tensor(observations, dtype = torch.float32), torch.tensor(actions, dtype = torch.float32)) # Create your dataset
+    dataloader = DataLoader(dataset, batch_size=128, shuffle=True) # Create your dataloader
+    
+    # TODO: Complete the training loop here ###
+    
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+        for obs_batch, action_batch in dataloader:
+            optimizer.zero_grad()
+            predicted_actions = learner(obs_batch)
+            loss = loss_fn(predicted_actions, action_batch)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(dataloader)}")
+    
+    return learner
+
+
+
+import torch
+import torch.nn as nn
+
+def get_checkpoint_path():
+    """Return the path to save the best performing model checkpoint.
+    
+    Returns:
+        checkpoint_path (str)
+            The path to save the best performing model checkpoint
+    """
+    return 'best_model_checkpoint.pth'
+
+class LinearRegression(nn.Module):
+    def __init__(self, input_dim=1, output_dim=1):
+        super().__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.linear(x)
+        
+
+def create_loss_and_optimizer(model):
+    """Create and return a loss function and optimizer.
+    
+    Parameters:
+        model (torch.nn.Module)
+            A neural network
+        learning_rate (float)
+            Learning rate for the optimizer
+    
+    Returns:
+        loss_fn (function)
+            The loss function for the model
+        optimizer (torch.optim.Optimizer)
+            The optimizer for the model
+    """
+    # TODO: Implement
+    lr=0.05
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr)
+    return loss_fn, optimizer
+
+def train(x, y, model, loss_fn, optimizer, checkpoint_path, num_epochs=1000):
+    """Train a model.
+    
+    Parameters:
+        x (torch.Tensor)
+            The input data
+        y (torch.Tensor)
+            The expected output data
+        model (torch.nn.Module)
+            A neural network
+        loss_fn (function)
+            The loss function
+        optimizer (torch.optim.Optimizer)
+            The optimizer for the model
+        checkpoint_path (str)
+            The path to save the best performing checkpoint
+        num_epochs (int)
+            The number of epochs to train for
+    
+    Side Effects:
+        - Save the best performing model checkpoint to `checkpoint_path`
+    """
+    # TODO: Implement
+    best_loss = float('inf')
+
+    for epoch in range(num_epochs):
+        predictions = model(x)
+        loss = loss_fn(predictions, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # print(loss)
+        if loss < best_loss:
+            best_loss = loss
+            torch.save(model.state_dict(), checkpoint_path)
+
+
+def load_model_checkpoint(checkpoint_path):
+    """Load a model checkpoint from disk.
+
+    Parameters:
+        checkpoint_path (str)
+            The path to load the checkpoint from
+    
+    Returns:
+        model (torch.nn.Module)
+            The model loaded from the checkpoint
+    """
+    # TODO: Implement
+    model = LinearRegression()
+    model.load_state_dict(torch.load(checkpoint_path))
+    return model
